de09d35c318d6f74ea7520b9b373699033671a9a
igpriyanshunegi <igpriyanshunegi@gmail.com>
2025-12-13 14:13:47 +0530

    setup database and create student function

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -12,16 +12,22 @@ import (
 
 	"github.com/igpriyanshunegi/students-api/internal/config"
 	"github.com/igpriyanshunegi/students-api/internal/http/handlers/student"
+	"github.com/igpriyanshunegi/students-api/internal/storage/sqlite"
 )
 
 func main() {
 	// load config
 	cfg := config.MustLoad()
 	// database setip
+	storage, err := sqlite.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	slog.Info("Storage Initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
 	// setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("POST /api/students", student.New())
+	router.HandleFunc("POST /api/students", student.New(storage))
 
 	// setup server
 	server := http.Server{
@@ -52,7 +58,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err := server.Shutdown(ctx)
+	err = server.Shutdown(ctx)
 	if err != nil {
 		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
 	}
--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -9,11 +9,12 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/igpriyanshunegi/students-api/internal/storage"
 	"github.com/igpriyanshunegi/students-api/internal/types"
 	"github.com/igpriyanshunegi/students-api/internal/utils/response"
 )
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("Creating a Student")
 		var student types.Student
@@ -39,6 +40,19 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "student created successfully"})
+		lastId, err := storage.CreateStudent(
+			student.Name,
+			student.Email,
+			student.Age,
+		)
+
+		slog.Info("user created Successfully", slog.String("user id", fmt.Sprint(lastId)))
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return
+
+		}
+
+		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
 	}
 }
